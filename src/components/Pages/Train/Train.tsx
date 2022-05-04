5bca109154557309056af5d7fc06aee6a4625056
rasul-surname <rasul.surname@mail.ru>
2022-05-04 14:29:36 +0300

    task-7 сделал первую задачу по умолчанию активной для раздела "Тренировка"

--- src/components/Pages/Train/Train.tsx
+++ src/components/Pages/Train/Train.tsx
@@ -6,11 +6,7 @@ import DisplayComponent from "./DisplayComponent";
 import BtnComponent from "./BtnComponent";
 import CardsTasks from "./CardsTasks/CardsTasks";
 import SplashScreen from "./SplashScreen/SplashScreen";
-import {
-    changeTime,
-    nextStepTime,
-    resetTime
-} from "../../../store/action_creators/list";
+import {changeTime, nextStepTime, resetTime} from "../../../store/action_creators/list";
 
 const Train: React.FC = () => {
     const {tasksOpen} = useTypedSelector(state => state.listReducer);
@@ -20,6 +16,12 @@ const Train: React.FC = () => {
     const [visibleBtn, setVisibleBtn] = useState<boolean>(true);
     const [activeTask, setActiveTask] = useState(-1);
     const [visibleTimer, setVisibleTimer] = useState(false);
+
+    useEffect(() => {
+        if(tasksOpen.length !== 0) {
+            connectTime(tasksOpen[0]);
+        }
+    }, []);
 
     function connectTime(elem: any) {
         setActiveTask(elem.id);
