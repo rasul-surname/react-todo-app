318470fb84b29cc39fdd73c35fc3c17b5d9d95f4
rasul-surname <rasul.surname@mail.ru>
2022-05-12 20:03:55 +0300

    task-11 добавил видимость pomodoro для задачи в тренировках

--- src/components/Pages/Train/CardsTasks/Card/Card.tsx
+++ src/components/Pages/Train/CardsTasks/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTypedSelector } from '../../../../../hooks/useTypedSelector';
 import classes from './Card.module.css';
 
 interface InterfaceCard {
@@ -9,11 +10,15 @@ interface InterfaceCard {
 }
 
 const Card: React.FC<InterfaceCard> = (props) => {
+	const {listTasks} = useTypedSelector(state => state.listReducer);
     const {id, title, onClick, activeTask} = props;
     const active = id === activeTask;
 
     return (
         <div className={active ? classes.box + ' ' + classes.active : classes.box} onClick={() => onClick()} >
+			<div className={active ? classes.boxTime + ' ' + classes.active : classes.boxTime}>
+				{listTasks[id - 1].minutes}
+			</div>
             <p>{title}</p>
         </div>
     );
