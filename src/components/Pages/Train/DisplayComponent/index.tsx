5a572b78690ea6a072a0ab25c0f50afd34997cb7
rasul-surname <rasul.surname@mail.ru>
2022-05-18 17:19:55 +0300

    task-15 добавил круговой бар для таймера

--- src/components/Pages/Train/DisplayComponent/index.tsx
+++ src/components/Pages/Train/DisplayComponent/index.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {useTypedSelector} from "../../../../hooks/useTypedSelector";
-
+import {buildStyles, CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import classes from './index.module.css';
 
 const DisplayComponent: React.FC = () => {
     const {time} = useTypedSelector(state => state.timeReducer);
     const {seconds, minutes} = time;
+    const [changingSeconds, setChangingSeconds] = useState(minutes * 60);
+
+    useEffect(() => {
+        setChangingSeconds(changingSeconds - 1);
+    }, [seconds]);
+
+    const timerMinutes = minutes >= 10 ? minutes : "0" + minutes;
+    const timerSeconds = seconds >= 10 ? seconds : "0" + seconds;
+
+    const totalSeconds = useMemo(() => {
+        return minutes * 60;
+    }, []);
+
+    const percentage = (changingSeconds / totalSeconds * 100).toFixed(5);
+
+    const red = '#f54e4e';
 
     return (
         <div className={classes.wrapper}>
-            <div className={classes.time}>{minutes >= 10 ? minutes : "0" + minutes}</div>&#58;
-            <div className={classes.time}>{seconds >= 10 ? seconds : "0" + seconds}</div>
+            <div className={classes.timer}>
+                <CircularProgressbar value={+percentage} text={timerMinutes + ' : ' + timerSeconds} styles={buildStyles({
+                    textColor: '#000',
+                    pathColor: red,
+                    trailColor: '#ECECEC',
+                })} />
+            </div>
         </div>
     );
 }
